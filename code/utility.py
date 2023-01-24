ffef272345fdcf54b17324886a9497e4b0486755
Ashkan <AshkanGanjE@gmail.com>
2023-01-24 15:07:42 -0500

    delete missplace tiles

--- code/utility.py
+++ code/utility.py
@@ -1,11 +1,3 @@
-
-def get_mis_tiles(arr,goal):
-    count = 0
-    for i in range(len(arr)):
-        if arr[i] != goal[i]:
-            count = count + 1
-    return count - 1
-        
 def write_data(data,file):
     with open(file, 'w') as f:
         for item in data:
