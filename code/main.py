2250db43e8d2feeabbfc1a3aeef9386d62baae9b
Lauren <lestanley48@gmail.com>
2023-02-07 13:06:02 -0500

    Update main.py

--- code/main.py
+++ code/main.py
@@ -7,37 +7,44 @@ from puzzle import Puzzle
 import utility
 import astar
 
-os.system('cls')
+def get_final_solution_state(initial_state, size, heuristic):
+    final_states = utility.get_final_states(initial_state, size)
+    final_state_blanks_at_end = final_states[0]
+    final_state_blanks_at_beginning = final_states[1]
+    initial_state = Node(initial_state)
+    final_state_blanks_at_end = Node(final_state_blanks_at_end)
+    final_state_blanks_at_beginning = Node(final_state_blanks_at_beginning)
 
-start = timeit.default_timer()
+    distance0 = Distance.calculate(
+        initial_state.get_current_state(), final_state_blanks_at_end.get_current_state(), heuristic, size)
+    distance1 = Distance.calculate(
+        initial_state.get_current_state(), final_state_blanks_at_beginning.get_current_state(), heuristic, size)
 
-heuristic = int(input(
-    "Choose a Heuristic: \n 1. Manhattan Distance \n 2. Manhattan By Weight Distance \n Enter : "))
+    if distance1 < distance0:
+        distance = distance1
+        final_state = final_state_blanks_at_beginning
+    else:
+        distance = distance0
+        final_state = final_state_blanks_at_end
+    return initial_state, final_state, distance
 
-# For reading the initial state from csv as matrix
-# Size is an array in the format: [HEIGHT, WIDTH]
-initial_state, size = utility.read_data_csv('./board1.csv')
-final_states = utility.get_final_states(initial_state, size)
-# To Do: implement a* for multiple solutions  
-final_state_blanks_at_end = final_states[0]
-final_state_blanks_at_beginning = final_states[1]
+def main_function():
+    #Initialize
+    os.system('cls')
+    start = timeit.default_timer()
+    # TODO Update to match specified format
+    heuristic = int(input("Choose a Heuristic: \n 1. Manhattan Distance \n 2. Manhattan By Weight Distance \n Enter : "))
 
-initial_state = Node(initial_state)
-final_state_blanks_at_end = Node(final_state_blanks_at_end)
-final_state_blanks_at_beginning = Node(final_state_blanks_at_beginning)
+    # For reading the initial state from csv as matrix
+    # Size is an array in the format: [HEIGHT, WIDTH]
+    initial_state, size = utility.read_data_csv('./board1.csv')
 
-distance0 = Distance.calculate(
-    initial_state.get_current_state(), final_state_blanks_at_end.get_current_state(), heuristic, size)
-distance1 = Distance.calculate(
-    initial_state.get_current_state(), final_state_blanks_at_beginning.get_current_state(), heuristic, size)
+    # Get final state and update to node format
+    initial_state, final_state, distance = get_final_solution_state(initial_state,size, heuristic)
+       
 
-if distance1 < distance0:
-    distance = distance1
-    final_state = final_state_blanks_at_beginning
-else:
-    distance = distance0
-    final_state = final_state_blanks_at_end
+    All_states, stop = astar.astar(initial_state, final_state, distance, heuristic, size)
+    print("all : ", len(All_states))
+    print('Time: ', stop - start)
 
-All_states, stop = astar.astar(initial_state, final_state, distance, heuristic, size)
-print("all : ", len(All_states))
-print('Time: ', stop - start)
+main_function()
