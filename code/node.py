bc7a0e6080511e2cbcdb6dc035fbd747c5cfe3ba
Lauren <lestanley48@gmail.com>
2023-02-09 20:22:46 -0500

    Cleaning Up

--- code/node.py
+++ code/node.py
@@ -13,7 +13,6 @@ class Node:
         self.hn = 0  # heuristic
         self.fn = 0  # evaluator
         self.move = "Start."
-        self.branching_factor = 0
 
     def get_parent(self):
         return self.parent
@@ -32,9 +31,6 @@ class Node:
 
     def get_node_move(self):
         return self.move
-    
-    def get_branching_factor(self):
-        return self.branching_factor
 
     def update_gn(self, gn):
         self.gn = gn
@@ -48,8 +44,6 @@ class Node:
     def update_move(self, move):
         self.move = move
     
-    def update_branching_factor(self, branching_factor):
-        self.branching_factor = branching_factor
 
     @staticmethod
     def update_expanded(goal_node, heuristic, size, current_node, fringe,node_copy, a, g, move_string):
@@ -165,7 +159,6 @@ class Node:
                         count = count +1
                         return best_node, distance, count
                             
-        #print('Branching Factor: ',branching_factor)
         #print('Distance: ', distance)
         #print('Best Node: ',best_node.get_current_state())
         return best_node, distance, count
@@ -174,13 +167,11 @@ class Node:
         a = [list(item.get_current_state()) for item in explored_nodes]
         explored_nodes.append(current_node)
         current_node_array = np.asarray(current_node.get_current_state())
-        branching_factor = 0
         for blank_space_index in blank_spaces : # considered blankspace as a list of index with 0 value
             #if item - size[0]*size[1] >=0:   # size is size of matrix
             if blank_space_index+1 > size[1]: #blank space is not on top layer
                 if current_node_array[blank_space_index - size[1]] != 0:
                     #print('not on top')
-                    branching_factor += 1
                     switch_tile = current_node_array[blank_space_index - size[1]]
                     node_copy = current_node_array.copy()
                     move = Movements(node_copy, current_node_array, blank_space_index,size)
@@ -194,7 +185,6 @@ class Node:
             if blank_space_index+1 < size[0]*size[1]+1 - size[1]: #blank space is not on bottom layer
                 if current_node_array[blank_space_index + size[1]] != 0:
                     #print('not on bottom')
-                    branching_factor += 1
                     switch_tile = current_node_array[blank_space_index + size[1]]
                     node_copy = current_node_array.copy()
                     move = Movements(node_copy, current_node_array, blank_space_index,size)
@@ -207,7 +197,6 @@ class Node:
 
             if blank_space_index % size[0] > 0:
                 if current_node_array[blank_space_index - 1] != 0:
-                    branching_factor += 1
                     switch_tile = current_node_array[blank_space_index - 1]
                     node_copy = current_node_array.copy()
                     move = Movements(node_copy, current_node_array, blank_space_index,size)
@@ -220,7 +209,6 @@ class Node:
 
             if (blank_space_index + 1) % size[0] != 0:
                 if current_node_array[blank_space_index + 1] != 0: 
-                    branching_factor += 1
                     switch_tile = current_node_array[blank_space_index + 1]
                     node_copy = current_node_array.copy()
                     move = Movements(node_copy, current_node_array, blank_space_index,size)
@@ -230,22 +218,17 @@ class Node:
                     #print(move_string)
                     Node.update_expanded(goal_node, heuristic, size, current_node, fringe, node_copy, a, g, move_string)
                     count = count + 1
-        print('Branching Factor: ',branching_factor)
-        current_node.update_branching_factor(branching_factor)
         return count
 
     def expand_node_greedy_astar(fringe, explored_nodes, current_node, goal_node, blank_spaces, g, count, heuristic, size):
             a = [list(item.get_current_state()) for item in explored_nodes]
             explored_nodes.append(current_node)
             current_node_array = np.asarray(current_node.get_current_state())
-            branching_factor = 0
-            #heuristic = [3,heuristic]
             for blank_space_index in blank_spaces : # considered blankspace as a list of index with 0 value
                 #if item - size[0]*size[1] >=0:   # size is size of matrix
                 if blank_space_index+1 > size[1]: #blank space is not on top layer
                     if current_node_array[blank_space_index - size[1]] != 0:
                         #print('NOT ON TOP')
-                        branching_factor += 1
                         switch_tile = current_node_array[blank_space_index - size[1]]
                         node_copy = current_node_array.copy()
                         move = Movements(node_copy, current_node_array, blank_space_index,size)
@@ -259,7 +242,6 @@ class Node:
                 if blank_space_index+1 < size[0]*size[1]+1 - size[1]: #blank space is not on bottom layer
                     if current_node_array[blank_space_index + size[1]] != 0:
                         #print('NOT ON BOTTOM')
-                        branching_factor += 1
                         switch_tile = current_node_array[blank_space_index + size[1]]
                         node_copy = current_node_array.copy()
                         move = Movements(node_copy, current_node_array, blank_space_index,size)
@@ -273,7 +255,6 @@ class Node:
                 if blank_space_index % size[0] > 0:
                     if current_node_array[blank_space_index - 1] != 0:
                         #print('NOT ON LEFT')
-                        branching_factor += 1
                         switch_tile = current_node_array[blank_space_index - 1]
                         node_copy = current_node_array.copy()
                         move = Movements(node_copy, current_node_array, blank_space_index,size)
@@ -287,7 +268,6 @@ class Node:
                 if (blank_space_index + 1) % size[0] != 0:
                     if current_node_array[blank_space_index + 1] != 0:
                         #print('NOT ON RIGHT') 
-                        branching_factor += 1
                         switch_tile = current_node_array[blank_space_index + 1]
                         node_copy = current_node_array.copy()
                         move = Movements(node_copy, current_node_array, blank_space_index,size)
@@ -297,7 +277,5 @@ class Node:
                         #print(move_string)
                         Node.update_expanded_greedy_star(goal_node, heuristic, size, current_node, fringe, node_copy, a, g, move_string)
                         count = count + 1
-            #print('Branching Factor: ',branching_factor)
-            current_node.update_branching_factor(branching_factor)
             return count
 
---
+++ code/npuzzle.py
@@ -0,0 +1,92 @@
+import timeit
+import numpy as np
+import os
+import sys
+from node import Distance
+from node import Node
+from puzzle import Puzzle
+import greedy
+import utility
+import astar
+
+# Call utility function to generate arrays for the two possible final states, and pick the closest to the starting position
+def get_final_solution_state(initial_state, size, heuristic):
+    final_states = utility.get_final_states(initial_state, size)
+    final_state_blanks_at_end = final_states[0]
+    final_state_blanks_at_beginning = final_states[1]
+    initial_state = Node(initial_state)
+    final_state_blanks_at_end = Node(final_state_blanks_at_end)
+    final_state_blanks_at_beginning = Node(final_state_blanks_at_beginning)
+
+    distance0 = Distance.calculate(
+        initial_state.get_current_state(), final_state_blanks_at_end.get_current_state(), heuristic, size)
+    distance1 = Distance.calculate(
+        initial_state.get_current_state(), final_state_blanks_at_beginning.get_current_state(), heuristic, size)
+
+    if distance1 < distance0:
+        distance = distance1
+        final_state = final_state_blanks_at_beginning
+    else:
+        distance = distance0
+        final_state = final_state_blanks_at_end
+    return initial_state, final_state, distance
+
+def get_final_solution_state_greedy(initial_state, size, heuristic):
+    final_states = utility.get_final_states(initial_state, size)
+    final_state_blanks_at_end = final_states[0]
+    final_state_blanks_at_beginning = final_states[1]
+    initial_state = Node(initial_state)
+    final_state_blanks_at_end = Node(final_state_blanks_at_end)
+    final_state_blanks_at_beginning = Node(final_state_blanks_at_beginning)
+
+    distance0 = greedy.greedy(initial_state, final_state_blanks_at_end, heuristic, size)
+    distance1 = greedy.greedy(initial_state, final_state_blanks_at_beginning, heuristic, size)
+
+    if distance1 < distance0:
+        distance = distance1
+        final_state = final_state_blanks_at_beginning
+    else:
+        distance = distance0
+        final_state = final_state_blanks_at_end
+    return initial_state, final_state, distance
+
+def main_function():
+    #Initialize
+    os.system('cls')
+    
+    #print(sys.argv)
+    # TODO Update to match specified format
+    board_file = sys.argv[1]
+    heuristic_specifier = sys.argv[2]
+    tile_weight = sys.argv[3]
+    heuristic = 1
+    if tile_weight == True:
+        heuristic = 2
+
+    # For reading the initial state from csv as matrix
+    # Size is an array in the format: [HEIGHT, WIDTH]
+    try:
+        initial_state, size = utility.read_data_csv('./'+str(board_file))
+        # Get final state and update to node format
+       
+        
+        if heuristic_specifier == 'sliding':
+            # Run A* Using the sliding tile heuristic
+            initial_state_node, final_state_node, distance = get_final_solution_state(initial_state,size, heuristic)
+            start = timeit.default_timer()
+            astar.astar(initial_state_node, final_state_node, distance, heuristic, size)
+            stop = timeit.default_timer()
+            print('Time: ', stop - start)
+        elif heuristic_specifier == 'greedy':
+            # Run A* with the greedy heuristic
+            initial_state_node, final_state_node, distance = get_final_solution_state_greedy(initial_state,size, heuristic)
+            start = timeit.default_timer()   
+            astar.astar_greedy(initial_state_node, final_state_node, distance, heuristic, size)
+            stop = timeit.default_timer()
+            print('Time: ', stop - start)
+        else: 
+            print('Heuristic invalid. Please run: \n npuzzle.py board1.csv sliding true \n or \n npuzzle.py board1.csv greedy true.')
+    except FileNotFoundError:
+        print('File not found. Please run: \n npuzzle.py board1.csv sliding true \n with a valid board file.')
+    
+main_function()
